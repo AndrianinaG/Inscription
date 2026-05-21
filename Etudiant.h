c386ea32a9685773bad9ca2f03b8259d1913f820
AndrianinaG <geoffroyandrianina@gmail.com>
2026-05-21 15:08:03 +0300

    Fix include formatting and add newlines

--- Etudiant.h
+++ Etudiant.h
@@ -1,19 +1,23 @@
 #ifndef ETUDIANT_H
 #define ETUDIANT_H
 
-#include<QString>
+#include <QString>
 
 class Etudiant
 {
 private:
     QString e_prenom;
     int e_age;
+
 public:
     Etudiant();
     ~Etudiant();
+
     void setPrenom(QString prenom);
     void setAge(int age);
+
     QString getPrenom();
     int getAge();
 };
+
 #endif // ETUDIANT_H
