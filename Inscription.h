9bd3bea5765d92520e37174ba2d9b2d59b8da5a0
AndrianinaG <geoffroyandrianina@gmail.com>
2026-05-21 15:09:16 +0300

    Format and align Inscription class members

--- Inscription.h
+++ Inscription.h
@@ -1,19 +1,22 @@
 #ifndef INSCRIPTION_H
 #define INSCRIPTION_H
-#include"Etudiant.h"
-#include<vector>
+
+#include "Etudiant.h"
+#include <vector>
 
 using namespace std;
+
 class Inscription
 {
-    public:
-        vector<Etudiant> liste;
+public:
+    vector<Etudiant> liste;
 
-        Inscription();
-        ~Inscription();
-        void ajouter(Etudiant student);
-        vector<Etudiant> getList();
-        int getSize();
+    Inscription();
+    ~Inscription();
+
+    void ajouter(Etudiant student);
+    vector<Etudiant> getList();
+    int getSize();
 };
 
 #endif // INSCRIPTION_H
